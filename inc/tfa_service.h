13a67a53e1b67e83935850797233f52176ec9e86
UtsavisGreat <utsavbalar1231@gmail.com>
2019-12-10 20:32:46 +0530

    tfa98xx: Import Xiaomi changes
    - Extracted from MiCode/vendor_qcom_opensource_audio-kernel
      at branch 'cepheus-q-oss'
    
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- inc/tfa_service.h
+++ inc/tfa_service.h
@@ -27,9 +27,6 @@ extern "C" {
 #endif
 
 /* Linux kernel module defines TFA98XX_GIT_VERSIONS in the linux_driver/Makefile */
-#if !defined(TFA98XX_GIT_VERSIONS)
-#include "versions.h"
-#endif
 #ifdef TFA98XX_GIT_VERSIONS
   #define TFA98XX_API_REV_STR "v6.7.4"/*TFA98XX_GIT_VERSIONS*/
 #else
